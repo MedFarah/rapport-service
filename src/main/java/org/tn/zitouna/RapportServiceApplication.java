891f20d8cbf39f6ad2177cb4f697510267e1d7c2
Mohamed Farah <hamouchka7@gmail.com>
2022-05-09 12:10:18 +0100

    first commit

---
+++ src/main/java/org/tn/zitouna/RapportServiceApplication.java
@@ -0,0 +1,13 @@
+package org.tn.zitouna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapportServiceApplication.class, args);
+	}
+
+}
